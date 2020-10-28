550645c2c37b07adfdf7de8ad12f82b1e1295514
v-verner <50997326+v-verner@users.noreply.github.com>
2020-10-28 11:33:55 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const storeList = document.querySelector("#wpsl-stores ul");
+if (storeList) {
+  storeList.addEventListener("DOMNodeInserted", (event) => {
+    if (event.target.nodeName === "#text") return false;
+    if (event.target.classList.contains("wpsl-preloader")) return false;
+    updateWhatsAppPhone(event.target);
+  });
+}
+
+function updateWhatsAppPhone(item) {
+  const contactItems = item.querySelector(".wpsl-contact-details");
+  if (!contactItems) return;
+
+  contactItems.childNodes.forEach((contact) => {
+    if (contact.nodeName === "#text") return;
+    const isWhatsApp = contact.innerText.search("WhatsApp");
+
+    if (isWhatsApp >= 0) {
+      const whatsAppNode = contact.childNodes[1];
+      let number = whatsAppNode.nodeValue;
+      number = number.replace(/\D/g, "");
+
+      const a = document.createElement("a");
+      a.innerText = whatsAppNode.nodeValue;
+      a.title = "Fale por WhatsApp";
+      a.href = "https://wa.me/55" + number;
+      a.target = "_blank";
+      contact.appendChild(a);
+      whatsAppNode.remove();
+    }
+  });
+}
